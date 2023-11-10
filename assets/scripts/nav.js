b1dcf9ca6a2266c6614a4c32e64abcc1bdf25d29
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-11-10 21:09:38 +0200

    Create base logic to switch between nav tabs

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,15 +1,54 @@
 const navTabs = document.getElementsByClassName("nav__tab");
 
+const previews = document.getElementsByClassName("home__preview");
+
+const previewsGames = document.getElementsByClassName("home__preview--game");
+const previewsExperiments = document.getElementsByClassName("home__preview--experiment");
+const previewsWebsites = document.getElementsByClassName("home__preview--website");
+
+
 initTabs();
 
 function initTabs() {
-    navTabs[0].classList.add("nav__tab--active")
+    setTabActive(navTabs[0])
+    setPreviewActive(previewsGames)
     
+    setPreviewInactive(previewsExperiments)
+    setPreviewInactive(previewsWebsites)
+
     for(let i = 0; i < navTabs.length; i++){
         let navTab = navTabs[i];
     
         navTab.addEventListener('click', function(){
             setTabActive(navTab)
+
+            console.log(navTab.innerHTML)
+
+            switch(navTab.innerHTML){
+                case "Video Games":
+                    setPreviewActive(previewsGames)
+
+                    setPreviewInactive(previewsExperiments)
+                    setPreviewInactive(previewsWebsites)
+                    break;
+                
+                case "Experiments":
+                    setPreviewActive(previewsExperiments)
+
+                    setPreviewInactive(previewsGames)
+                    setPreviewInactive(previewsWebsites)
+                break;
+
+                case "Websites":
+                    setPreviewActive(previewsWebsites)
+
+                    setPreviewInactive(previewsGames)
+                    setPreviewInactive(previewsExperiments)
+                    break;
+
+                default :
+                    console.error("We didnt find the previews to show")
+            }
         }, false);
     }
 }
@@ -24,4 +63,21 @@ function setTabActive(navTab) {
 
 function setTabInactive(navTab) {
     navTab.classList.remove("nav__tab--active");
+}
+
+
+function setPreviewActive(previews) {
+    console.log("Activating these previews :", previews)
+
+    for(let i = 0; i < previews.length; i++){
+        previews[i].classList.remove("home__preview--inactive");
+    }
+}
+
+function setPreviewInactive(previews) {
+    console.log("Desactivating these previews :", previews)
+
+    for(let i = 0; i < previews.length; i++){
+        previews[i].classList.add("home__preview--inactive");
+    }
 }
